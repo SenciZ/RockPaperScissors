4959f154a0bbed0ca0e6cebcc0a0cfdd9ba945ea
SenciZ <zenkovic88@gmail.com>
2021-03-14 22:49:14 -0500

    Rock paper scissors start ui

--- app.js
+++ app.js
@@ -1,72 +1,75 @@
+let playerWins = 0;
+let computerWins = 0;
+
+
+
+const buttonClicked = document.querySelectorAll("button");
+buttonClicked.forEach(buttonClicked  => {
+    buttonClicked.addEventListener("click", (e) => {
+        let playerSelection = e.target.className;
+        let computerSelection = computerPlay()
+        playRound(playerSelection, computerSelection);
+            if ((playerWins === 5) || (computerWins === 5)){
+                finalScore();
+            } 
+    });
+});
+
 function computerPlay(){
     const rpsChoice = ["rock", "paper", "scissors"];
     let randomNumber = Math.floor(Math.random()*3);
     return rpsChoice[randomNumber];
 }
 
-let playerWins=0;
-let computerWins=0;
+
 
 function playRound(playerSelection, computerPlay){
+    let gameWinner = document.querySelector(".gameWinner");
     if (playerSelection === "rock" && computerPlay === "scissors"){
         playerWins++
-        return "You Win! Rock beats Scissors";
+        gameWinner.textContent = "You Win! Rock beats Scissors";
     } else if (playerSelection === "paper" && computerPlay === "rock"){
         playerWins++
-        return "You Win! Paper beats Rock!";
+        gameWinner.textContent = "You Win! Paper beats Rock!";
     } else if (playerSelection === "scissors" && computerPlay === "paper"){
         playerWins++
-        return "You Win! Scissors beats Paper!";
+        gameWinner.textContent = "You Win! Scissors beats Paper!";
     } else if (computerPlay === "scissors" && playerSelection === "paper"){
         computerWins++
-        return "You Lose! Scissors beats Paper!";
+        gameWinner.textContent = "You Lose! Scissors beats Paper!";
     } else if (computerPlay === "paper" && playerSelection === "rock"){
         computerWins++
-        return "You Lose! Paper beats Rock!";
+        gameWinner.textContent = "You Lose! Paper beats Rock!";
     } else if (computerPlay === "rock" && playerSelection === "scissors"){
         computerWins++
-        return "You Lose! Rock beats Scissors";
+        gameWinner.textContent = "You Lose! Rock beats Scissors";
     } else {
-        return "Its a Draw!";
+        gameWinner.textContent = "Its a Draw!";
     }
 }
-let winnerofRound;
 
-function game(){
-    let playerSelection = prompt("Enter Rock, Paper or Scissor").toLowerCase();
-    let computerSelection = computerPlay()
-    console.log(playRound(playerSelection, computerSelection));
-
-    playerSelection = prompt("Enter Rock, Paper or Scissor").toLowerCase();
-    computerSelection = computerPlay()
-    console.log(playRound(playerSelection, computerSelection));
-        
-    playerSelection = prompt("Enter Rock, Paper or Scissor").toLowerCase();
-    computerSelection = computerPlay()
-    console.log(playRound(playerSelection, computerSelection));
-        
-    playerSelection = prompt("Enter Rock, Paper or Scissor").toLowerCase();
-    computerSelection = computerPlay()
-    console.log(playRound(playerSelection, computerSelection));
-        
-    playerSelection = prompt("Enter Rock, Paper or Scissor").toLowerCase();
-    computerSelection = computerPlay()
-    console.log(playRound(playerSelection, computerSelection));
-
-    finalScore();
-}
-
-game();
 
 function finalScore(){
+    let gameCounter = document.querySelector(".gamecount");
+
     if (playerWins > computerWins){
-        console.log("You have won!")
+        gameCounter.textContent = "You Win!";
+        disableButtons();
+
     } else if (playerWins<computerWins){
-        console.log("Computer Wins!")
+        gameCounter.textContent = "Computer Wins!";
+        disableButtons();
+
     } else if (playerWins === computerWins){
-        console.log("Its a tie")
-    } else {
-    }
+        gameCounter.textContent = "Its a Tie!";
+        disableButtons();
+    }  
+}
+
+function disableButtons() {
+    buttonClicked.forEach(buttonClicked => {
+        buttonClicked.disabled = true
+    });
 }
 
 
